dcd8f25a73420bd444b4584ab1f7c58edb2240c1
Angela Rosas <gelita@users.noreply.github.com>
2019-05-09 09:10:01 -0700

    init commit

---
+++ app/src/main/java/com/fanikiosoftware/mynavigationdrawer/Controllers/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fanikiosoftware.mynavigationdrawer.Controllers.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fanikiosoftware.mynavigationdrawer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
